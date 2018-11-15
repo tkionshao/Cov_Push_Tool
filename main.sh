b1c7f5b92f3ce2b424ef19b175fd938bfbe22170
hen <hen@ghtinc.com>
2018-11-15 03:29:13 -0500

    201811150428

--- main.sh
+++ main.sh
@@ -3,8 +3,12 @@ source ./instance.cnf
 NOW=`date +%Y%m%d`
 # dump all in one sp
 read -p "Version? (ex:777_07.02 or SP_777)" SQLFILENAME
-sh ./bin/all_in_one_dump.sh ${SQLFILENAME}
+git pull
+git checkout ${BRANCHNAME}
 
+sh ./bin/all_in_one_dump.sh ${SQLFILENAME}
+zip -r ./sp_release/tmp_sql_file.zip ./sp_release/${SQLFILENAME}/
+ 
 # dump singles sp
 sh ./bin/make_single_dump_command.sh
 sh ./tmp/command_ready_to_single_dump.sh
