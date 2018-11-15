6b3b6e72f86d3d8b673fd266bd63bc195ede3a21
hen <hen@ghtinc.com>
2018-11-15 03:13:27 -0500

    hen

--- git_lab_push.sh
+++ git_lab_push.sh
@@ -2,7 +2,7 @@
 source ./instance.cnf
 read -p "commit description: " comment
 
-git pull origin ${BRANCHNAME}
+# git pull origin ${BRANCHNAME}
 git checkout ${BRANCHNAME}
 git add .
 git commit -m "${comment}"
