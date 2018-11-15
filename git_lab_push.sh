5173e95ebd791756d38c6894744182ae0175a201
hen <hen@ghtinc.com>
2018-11-15 02:11:41 -0500

    fix

--- git_lab_push.sh
+++ git_lab_push.sh
@@ -2,6 +2,10 @@
 source ./instance.cnf
 read -p "commit description: " comment
 
+git pull origin ${BRANCHNAME}
+git checkout ${BRANCHNAME}
 git add .
 git commit -m "${comment}"
 git push origin ${BRANCHNAME} 
+
+
