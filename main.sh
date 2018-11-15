e94f235683094917f99892d8c9b3e95fd18d6e0e
hen <hen@ghtinc.com>
2018-11-15 00:36:44 -0500

    tmp zip

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ NOW=`date +%Y%m%d`
 # dump all in one sp
 read -p "Version? (ex:777_07.02 or SP_777)" SQLFILENAME
 sh ./bin/all_in_one_dump.sh ${SQLFILENAME}
+zip -r ./sp_release/tmp_sql_file.zip ./sp_release/${SQLFILENAME}/
 
 # dump singles sp
 sh ./bin/make_single_dump_command.sh
