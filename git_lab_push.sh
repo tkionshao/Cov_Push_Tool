981c36797b97447a627fb408ae039af0cd785985
hen <hen@ghtinc.com>
2018-11-19 03:55:39 -0500

    777_09.02 - rm some folder with no data

--- git_lab_push.sh
+++ git_lab_push.sh
@@ -6,7 +6,7 @@ read -p "commit description: " comment
 #git pull origin ${BRANCHNAME}
 git checkout ${BRANCHNAME}
 git add .
-git commit -m "${VERSION}${comment}"
+git commit -m "${VERSION} - ${comment}"
 git tag -a ${VERSION} -m "${comment}"
 git push origin ${BRANCHNAME} 
 
