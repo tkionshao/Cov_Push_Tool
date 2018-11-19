a859dac7465f40c95e276a5a4d3e4d0109557ab9
hen <hen@ghtinc.com>
2018-11-19 03:44:36 -0500

    777_09.02

--- upload_to_sp_release/upload_to_internal_server.bat
+++
@@ -1,7 +0,0 @@
-RMDIR .\tmp /S
-MKDIR .\tmp
-pscp -pw covmo123 covmo@192.168.1.221:"/home/covmo/covmo_sp_main/sp_release/*.zip" .\tmp
-7za.exe x ".\tmp\tmp_sql_file.zip" -oc:.\tmp 
-DEL .\tmp\tmp_sql_file.zip
-XCOPY ".\tmp\sp_release\*" "\\192.168.3.242\Project3\CovMo\Module\Database\SP_Release\" /e
-pause
