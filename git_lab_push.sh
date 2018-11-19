8377548faedb4bc52a3921d5aa73d23d902f3e74
hen <hen@ghtinc.com>
2018-11-19 04:23:20 -0500

    SP_783 - first commit on git lab

--- git_lab_push.sh
+++ git_lab_push.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
+VERSION=$1
 source ./instance.cnf
 read -p "commit description: " comment
 
-# git pull origin ${BRANCHNAME}
+#git pull origin ${BRANCHNAME}
 git checkout ${BRANCHNAME}
 git add .
-git commit -m "${comment}"
+git tag -a ${VERSION} -m '${comment}'
+git commit -m "${VERSION} - ${comment}"
 git push origin ${BRANCHNAME} 
+
+
--- main.sh
+++ main.sh
@@ -3,12 +3,12 @@ source ./instance.cnf
 NOW=`date +%Y%m%d`
 # dump all in one sp
 read -p "Version? (ex:777_07.02 or SP_777)" SQLFILENAME
-git pull 
+git pull
 git checkout ${BRANCHNAME}
 
 sh ./bin/all_in_one_dump.sh ${SQLFILENAME}
-zip -r ./sp_release/tmp_sql_file.zip ./sp_release/${SQLFILENAME}/
-
+zip -r ./sp_release/tmp_sql_file.zip ./sp_release/${SQLFILENAME}
+ 
 # dump singles sp
 sh ./bin/make_single_dump_command.sh
 sh ./tmp/command_ready_to_single_dump.sh
@@ -21,4 +21,4 @@ echo "sql file: ${SQLFILENAME}"
 echo "time: ${NOW}"
 
 # git lab push
-sh ./git_lab_push.sh
+sh ./git_lab_push.sh $SQLFILENAME
