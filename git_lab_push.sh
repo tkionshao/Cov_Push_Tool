11800836a2bbc862ce3bdf28679f2e0c741d2ed3
hen <hen@ghtinc.com>
2018-11-19 04:02:38 -0500

    777_09.02_test - hen

--- git_lab_push.sh
+++ git_lab_push.sh
@@ -6,7 +6,7 @@ read -p "commit description: " comment
 #git pull origin ${BRANCHNAME}
 git checkout ${BRANCHNAME}
 git add .
-git tag -a ${VERSION} -m "${comment}"
+git tag -a ${VERSION} -m '${comment}'
 git commit -m "${VERSION} - ${comment}"
 git push origin ${BRANCHNAME} 
 
